1d91e8a15f9ca1cc31e1df9f72d80e0d52834716
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2021-05-03 23:56:47 -0700

    birds: Fix unicode render

--- birds/render.go
+++ birds/render.go
@@ -7,18 +7,24 @@ import (
 
 // RenderHTML renders the tweet text into HTML.
 func (t *Tweet) RenderHTML(photos Photos) string {
-	text := t.Text
-	// First, render all urls and mentions
+	// First, replace all urls and mentions
 	entities := t.Entities.sort()
+	runes := []rune(t.Text)
 	for _, e := range entities {
-		text = text[:e.getStart()] + e.render() + text[e.getEnd():]
+		replaced := runes[:e.getStart()]
+		replaced = append(replaced, []rune(e.render())...)
+		replaced = append(replaced, runes[e.getEnd():]...)
+		runes = replaced
 	}
+	text := string(runes)
+
 	// Then, add line breakers and get a whole p tag for the text
 	text = strings.ReplaceAll(text, "\n", "<br />\n")
 	var sb strings.Builder
 	sb.WriteString("<p>")
 	sb.WriteString(text)
 	sb.WriteString("</p>\n")
+
 	// Finally, add any photos
 	for _, key := range t.Attachments.MediaKeys {
 		if m, ok := photos[key]; ok {
@@ -31,5 +37,6 @@ func (t *Tweet) RenderHTML(photos Photos) string {
 			sb.WriteString("\n")
 		}
 	}
+
 	return sb.String()
 }
